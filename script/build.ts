c7e64c155e91d01f006b08ea6328e642f4624896
shrinathjaiswal70 <shrinathjaiswal70@gmail.com>
2026-02-22 22:15:56 -0800

    Update script/build.ts with Vite and esbuild configuration

--- script/build.ts
+++ script/build.ts
@@ -1,12 +1,10 @@
 import { build as viteBuild } from 'vite';
 import { build as esbuildBuild } from 'esbuild';
 
-// Paths
 const clientRoot = './client';
 const serverEntry = './server/index.ts';
 const outputDir = './dist';
 
-// Vite build for client
 viteBuild({
     root: clientRoot,
     outDir: `${outputDir}/public`,
@@ -16,7 +14,6 @@ viteBuild({
     console.error('Error during the Vite build:', err);
 });
 
-// Esbuild build for server
 esbuildBuild({
     entryPoints: [serverEntry],
     bundle: true,
